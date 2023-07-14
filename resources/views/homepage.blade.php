f3ac3b50e42288326edfcde755215c1638dcf12d
Programmer <36xitkj1@gmial.com>
2023-07-14 14:13:02 +0700

    update layout

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -17,7 +17,7 @@
                         <a href="#form"
                             class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-white rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800">
                             <span
-                                class="relative text-2xl px-16 py-2.5 transition-all ease-in duration-75 bg-gradient-to-r from-orange-500 to-pink-500 hover:bg-gradient-to-bl dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                                class="relative text-2xl px-12 py-2.5 transition-all ease-in duration-75 bg-gradient-to-r from-orange-500 to-pink-500 hover:bg-gradient-to-bl dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
                                 Daftar Sekarang
                             </span>
                         </a>
