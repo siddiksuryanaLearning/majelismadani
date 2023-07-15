46b46276fef109e4b3b05e7930fcfeb661b16119
Programmer <36xitkj1@gmial.com>
2023-07-15 15:12:07 +0700

    update

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -21,33 +21,6 @@
     <meta property="og:url" content="https://daftar.majelismadani.id/">
     {{-- end meta --}}
 
-    <!-- Primary Meta Tags -->
-    {{-- <title>Peresmian Masjid Al-Muttaqien Madrasah Karya Madani dan Gerakan
-        Shodaqoh Biaya Pendidikan</title>
-    <meta name="title"
-        content="Peresmian Masjid Al-Muttaqien Madrasah Karya Madani dan Gerakan
-Shodaqoh Biaya Pendidikan" />
-    <meta name="description" content="29 Juli 2023, Jam 06:00 s/d Selesai" />
-
-    <!-- Open Graph / Facebook -->
-    <meta property="og:type" content="website" />
-    <meta property="og:url" content="https://daftar.majelismadani.id/" />
-    <meta property="og:title"
-        content="Peresmian Masjid Al-Muttaqien Madrasah Karya Madani dan Gerakan
-Shodaqoh Biaya Pendidikan" />
-    <meta property="og:description" content="29 Juli 2023, Jam 06:00 s/d Selesai" />
-    <meta property="og:image" content="https://daftar.majelismadani.id/img/pemater_terupdate.png" />
-
-    <!-- Twitter -->
-    <meta property="twitter:card" content="summary_large_image" />
-    <meta property="twitter:url" content="https://daftar.majelismadani.id/" />
-    <meta property="twitter:title"
-        content="Peresmian Masjid Al-Muttaqien Madrasah Karya Madani dan Gerakan
-Shodaqoh Biaya Pendidikan" />
-    <meta property="twitter:description" content="29 Juli 2023, Jam 06:00 s/d Selesai" />
-    <meta property="twitter:image" content="https://daftar.majelismadani.id/img/pemater_terupdate.png" /> --}}
-
-    <!-- Meta Tags Generated with https://metatags.io -->
     <style>
         [x-cloak] {
             display: none !important;
