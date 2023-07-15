e3ef37aca28fb8b9b403b120c89b4e5949a312d8
Programmer <36xitkj1@gmial.com>
2023-07-15 16:39:02 +0700

    update lagi

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -9,7 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ config('app.name') }}</title>
     {{-- meta --}}
-    <meta property="og:site_name" content="Madrasah Karya Madani">
+    {{-- <meta property="og:site_name" content="Madrasah Karya Madani">
     <meta property="og:title"
         content="Peresmian Masjid Al-Muttaqien Madrasah Karya Madani dan Gerakan Shodaqoh Biaya Pendidikan">
     <meta property="og:description" content="Silturahmi Akbar">
@@ -17,8 +17,31 @@
         content="https://sidiksuryanass.files.wordpress.com/2023/07/safeimagekit-resized-img-1.png">
     <meta property="og:type" content="website">
     <meta property="og:image:type" content="image/png">
-    <meta property="og:url" content="https://daftar.majelismadani.id/">
+    <meta property="og:url" content="https://daftar.majelismadani.id/"> --}}
     {{-- end meta --}}
+    <meta name="description"
+        content="Peresmian Masjid Al-Muttaqien Madrasah Karya Madani dan Gerakan Shodaqoh Biaya Pendidikan">
+    <link rel="canonical" href="https://daftar.majelismadani.id/">
+    <meta property="og:locale" content="en_US">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="Cek Mutasi Bank Secara Otomatis Rekening Indonesia - Moota.co">
+    <meta property="og:description"
+        content="Peresmian Masjid Al-Muttaqien Madrasah Karya Madani dan Gerakan Shodaqoh Biaya Pendidikan">
+    <meta property="og:url" content="https://daftar.majelismadani.id/>
+    <meta property="og:site_name"
+        content="Moota.co">
+    <meta property="article:publisher" content="https://www.facebook.com/appmoota/">
+    <meta property="article:modified_time" content="2022-11-25T09:13:17+00:00">
+    <meta property="og:image"
+        content="https://sidiksuryanass.files.wordpress.com/2023/07/safeimagekit-resized-img-1.png">
+    <meta property="og:image:width" content="200">
+    <meta property="og:image:height" content="200">
+    <meta property="og:image:type" content="image/png">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:label1" content="Est. reading time">
+    <meta name="twitter:data1" content="16 minutes">
+
+
 
     <style>
         [x-cloak] {
