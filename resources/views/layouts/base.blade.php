f7acf682f13f1f50aab39f37fffcd4fd679de2b1
Programmer <36xitkj1@gmial.com>
2023-07-15 14:53:58 +0700

    update

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -30,21 +30,21 @@ Shodaqoh Biaya Pendidikan" />
 
     <!-- Open Graph / Facebook -->
     <meta property="og:type" content="website" />
-    <meta property="og:url" content="https://metatags.io" />
+    <meta property="og:url" content="https://daftar.majelismadani.id/" />
     <meta property="og:title"
         content="Peresmian Masjid Al-Muttaqien Madrasah Karya Madani dan Gerakan
 Shodaqoh Biaya Pendidikan" />
     <meta property="og:description" content="29 Juli 2023, Jam 06:00 s/d Selesai" />
-    <meta property="og:image" content="https://metatags.io/images/meta-tags.png" />
+    <meta property="og:image" content="https://daftar.majelismadani.id/img/pemater_terupdate.png" />
 
     <!-- Twitter -->
     <meta property="twitter:card" content="summary_large_image" />
-    <meta property="twitter:url" content="https://metatags.io" />
+    <meta property="twitter:url" content="https://daftar.majelismadani.id/" />
     <meta property="twitter:title"
         content="Peresmian Masjid Al-Muttaqien Madrasah Karya Madani dan Gerakan
 Shodaqoh Biaya Pendidikan" />
     <meta property="twitter:description" content="29 Juli 2023, Jam 06:00 s/d Selesai" />
-    <meta property="twitter:image" content="https://metatags.io/images/meta-tags.png" />
+    <meta property="twitter:image" content="https://daftar.majelismadani.id/img/pemater_terupdate.png" />
 
     <!-- Meta Tags Generated with https://metatags.io -->
     <style>
