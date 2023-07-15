d911949b7133f327b033cd395c5d1fe54dcb163c
Programmer <36xitkj1@gmial.com>
2023-07-15 14:08:50 +0700

    add

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -24,7 +24,7 @@
         [x-cloak] {
             display: none !important;
         }
-    </style>>
+    </style>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @livewireStyles
     @livewireScripts
